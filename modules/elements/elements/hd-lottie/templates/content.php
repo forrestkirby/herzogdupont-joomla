e6e4172d1e1707be27ab2b300d44a2e25cbe33af
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2022-04-09 00:11:12 +0200

    add animation name to lottie element

--- modules/elements/elements/hd-lottie/templates/content.php
+++ modules/elements/elements/hd-lottie/templates/content.php
@@ -6,14 +6,14 @@ if ($props['path']) : ?>
 <?php
 	$uniqid = uniqid('hd-');
 	$loop = $props['loop'] == true ? true : false;
-	$path = isset($props['path']) ? $props['path'] : 'http://localhost/fuse/plugins/system/herzogdupont/modules/elements/elements/hd-lottie/assets/demo.json';
+	$path = $props['path'];
 ?>
 <?php if ($props['link']) : ?>
 <a href="<?= $props['link'] ?>">
 <?php endif ?>
 	<div id="<?= $uniqid ?>"></div>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.13/lottie.min.js" integrity="sha512-srGxQe2w7s50+5/nNgEVKYtBm15zRylJwdjxYnGEZr3mmHFJKFjA/ImA2OKizVzoIDX8XISMHDI1+az9pnumbQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.9.2/lottie.min.js" integrity="sha512-R47oG09eydaf225nXyDdSWJ8frKwxkC7QIHXdPpbUx9KaAiC6akjpWxTBw1hb7whk7P2sJNuRhS2ljSBegnIPA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <?php if ($props['link']) : ?>
 </a>
