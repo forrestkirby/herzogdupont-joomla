137e6ac8b24f06c27d3c6b9d57a138ac4f97aa4b
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2022-08-04 23:12:04 +0200

    improve settings layout

--- modules/elements/src/SettingsListener.php
+++ modules/elements/src/SettingsListener.php
@@ -14,65 +14,7 @@ class SettingsListener
 {
     public static function initCustomizer(Config $config)
     {
-        // Add settings panel
-        $config->set('customizer.panels.herzogdupont', [
-            'title'  => 'Herzog Dupont',
-            'width'  => 400,
-            'fields' => [
-                'hd.elements.hd-counter' => [
-                    'type' => 'checkbox',
-                    'label' => 'Counter element',
-                    'default' => true,
-                    'text' => 'Add to the element library',
-                ],
-                'hd.elements.hd-flipcard' => [
-                    'type' => 'checkbox',
-                    'label' => 'Flipcard element',
-                    'default' => true,
-                    'text' => 'Add to the element library',
-                ],
-                'hd.elements.hd-image-comparison' => [
-                    'type' => 'checkbox',
-                    'label' => 'Image Comparison element',
-                    'default' => true,
-                    'text' => 'Add to the element library',
-                ],
-                'hd.elements.hd-lottie' => [
-                    'type' => 'checkbox',
-                    'label' => 'Lottie element',
-                    'default' => true,
-                    'text' => 'Add to the element library',
-                ],
-                'hd.elements.hd-progress' => [
-                    'type' => 'checkbox',
-                    'label' => 'Progress element',
-                    'default' => true,
-                    'text' => 'Add to the element library',
-                ],
-                'hd.elements.hd-slideshow-grid' => [
-                    'type' => 'checkbox',
-                    'label' => 'Slideshow Grid element',
-                    'default' => true,
-                    'text' => 'Add to the element library',
-                ],
-                'hd.elements.hd-timeline' => [
-                    'type' => 'checkbox',
-                    'label' => 'Timeline element',
-                    'default' => true,
-                    'text' => 'Add to the element library',
-                ],
-                'hd.elements.hd-toggle' => [
-                    'type' => 'checkbox',
-                    'label' => 'Toggle element',
-                    'default' => true,
-                    'text' => 'Add to the element library',
-                ],
-            ],
-        ]);
-
-        $config->set('customizer.sections.settings.fields.settings.items.herzogdupont', 'Herzog Dupont');
-        
-        // Add style customizer section
+        // Add settings panel and style customizer components
         $config->addFile('customizer', Path::get('./customizer.json'));
 
         // Recompile LESS style on installation
