be43762d24bcf1be125359a24cfce5ba315baef4
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2022-09-15 22:28:30 +0200

    fix syntax

--- modules/elements/elements/hd-progress/templates/content.php
+++ modules/elements/elements/hd-progress/templates/content.php
@@ -2,7 +2,7 @@
 
 /* Herzog Dupont for YOOtheme Pro Copyright (C) 2018–2022 Thomas Weidlich GNU GPL v3 */
 
-if $props['content'] : ?>
+if ($props['content']) : ?>
 <div><?= $props['content'] ?></div>
 <?php endif ?>
 <progress value="<?= $props['stop'] ?>" max="<?= $props['max'] ?>"><?= $props['stop'] ?>/<?= $props['max'] ?></progress>
